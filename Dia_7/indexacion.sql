26f05b5ddf2a66a8ea5d408cc232ea0fa14899bf
Kenia <keniayhdiaz@gmail.com>
2024-07-04 22:11:03 -0500

    feat: :zap: Aplicando la indexacion en ejercicio pasado

---
+++ Dia_7/indexacion.sql
@@ -0,0 +1,27 @@
+-- ###################################
+-- ######### DIA 2 - MySQL 2 #########
+-- ###################################
+
+-- 1. Escribe una consulta que permita agrupar los clientes de todos los empleados
+-- cuyo puesto sea responsable de ventas. Se requiere que la consulta muestre:
+-- Nombre del cliente, teléfono, la ciudad, nombre y primer apellido del responsable
+-- de ventas y su email.
+
+
+
+-- 2- Se necesita obtener los registros de los pedidos entre el 15 de marzo del 2009 y
+-- el 15 de julio del 2009, para todos los clientes que sean de la ciudad de
+-- Sotogrande. Se requiere mostrar el código del pedido, la fecha del pedido, fecha
+-- de entrega, estado, los comentarios y el condigo del cliente que realizo dicho
+-- pedido.
+
+
+
+-- 3- Se desea obtener los productos cuya gama pertenezca a las frutas y que el
+-- proveedor sea Frutas Talaveras S.A, se desea mostrar el código, nombre,
+-- descripción, cantidad en stock, y su precio con 10% de descuento, de igual forma
+-- se pide la cantidad en los distintos pedidos que se han hecho.
+
+
+
+-- Desarrollado por Kenia Yulieth Hernández Díaz / ID.1.098.386.069
